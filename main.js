003183f04030de45d1a32e74519091e489b1a2e9
Ankush Yadav <yadavankush1811@gmail.com>
2025-10-07 16:00:49 +0530

    multiplication function is done.

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ const btn1 = document.getElementById('btn1').addEventListener('click', (e) => {
     num1 = e.target.value;
 });
 
-const btnAdd = document.getElementById('btn-add').addEventListener('click', (e) => {
+const btnAdd = document.getElementById('btn-add').addEventListener('click', () => {
     num += num1;
     document.getElementById('result').textContent = num;
     document.getElementById('num1').value = 0;
 });
 
-const btnSub = document.getElementById('btn-sub').addEventListener('click', (e) => {
+const btnSub = document.getElementById('btn-sub').addEventListener('click', () => {
     if (num1 > num) {
         num = num1 - num;
         document.getElementById('result').textContent = num;
@@ -27,6 +27,26 @@ const btnSub = document.getElementById('btn-sub').addEventListener('click', (e) 
     }
 })
 
+const btnmulti = document.getElementById('btn-multi').addEventListener('click', () => {
+    if (num == 0) {
+        num = 1;
+        num *= num1;
+        document.getElementById('result').textContent = num;
+        document.getElementById('num1').value = 0;
+    }
+    else {
+        num *= num1;
+        document.getElementById('result').textContent = num;
+        document.getElementById('num1').value = 0;
+    }
+})
+
+const btndiv = document.getElementById('btn-div').addEventListener('click', () => {
+    num = 0;
+    num /= num1;
+    document.getElementById('result').textContent = num;
+    document.getElementById('num1').value = 0;
+})
 const cls = document.getElementById('btn-cls').addEventListener('click', () => {
     document.getElementById('num1').value = 0;
     document.getElementById('result').textContent = 0;

34b12215ec9aeefa303a302ffceab106893c246a
Ankush Yadav <yadavankush1811@gmail.com>
2025-10-07 16:22:42 +0530

    all buttons are functioning now.

--- main.js
+++ main.js
@@ -1,12 +1,71 @@
 let num = 0;
+
 let num1 = document.getElementById('num1');
 num1.addEventListener('input', (e) => {
-    num1 = Number(e.target.value);
+    Number(e.target.value) = btn1;
 });
 
 const btn1 = document.getElementById('btn1').addEventListener('click', (e) => {
-    num1 = e.target.value;
+    document.getElementById('num1').value = e.target.value
+    num1 = Number(e.target.value);
+    console.log(num1);
 });
+
+const btn2 = document.getElementById('btn2').addEventListener('click', (e) => {
+    document.getElementById('num1').value = e.target.value
+    num1 = Number(e.target.value);
+    console.log(num1);
+});
+
+const btn3 = document.getElementById('btn3').addEventListener('click', (e) => {
+
+    document.getElementById('num1').value = e.target.value;
+    num1 = Number(e.target.value);
+    console.log(num1);
+});
+
+const btn4 = document.getElementById('btn4').addEventListener('click', (e) => {
+    document.getElementById('num1').value = e.target.value;
+    num1 = Number(e.target.value);
+    console.log(num1);
+});
+
+const btn5 = document.getElementById('btn5').addEventListener('click', (e) => {
+    document.getElementById('num1').value = e.target.value;
+    num1 = Number(e.target.value);
+    console.log(num1);
+});
+
+const btn6 = document.getElementById('btn6').addEventListener('click', (e) => {
+    document.getElementById('num1').value = e.target.value;
+    num1 = Number(e.target.value);
+    console.log(num1);
+});
+
+const btn7 = document.getElementById('btn7').addEventListener('click', (e) => {
+    document.getElementById('num1').value = e.target.value;
+    num1 = Number(e.target.value);
+    console.log(num1);
+});
+
+const btn8 = document.getElementById('btn8').addEventListener('click', (e) => {
+    document.getElementById('num1').value = e.target.value;
+    num1 = Number(e.target.value);
+    console.log(num1);
+});
+
+const btn9 = document.getElementById('btn9').addEventListener('click', (e) => {
+    document.getElementById('num1').value = e.target.value;
+    num1 = Number(e.target.value);
+    console.log(num1);
+});
+
+const btn0 = document.getElementById('btn0').addEventListener('click', (e) => {
+    document.getElementById('num1').value = e.target.value;
+    num1 = Number(e.target.value);
+    console.log(num1);
+});
+
 
 const btnAdd = document.getElementById('btn-add').addEventListener('click', () => {
     num += num1;
